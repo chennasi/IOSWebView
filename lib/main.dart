4400c2908090eb0f9ac1866f8a05bf471b1504c9
Chenna Subramani <chenna@chenna.co.in>
2026-02-10 05:41:56 +0530

    Add main Flutter app with WebViewPage
    Initial implementation of a Flutter app with a WebView.

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: WebViewPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri("https://yourwebsite.com"), // Change this URL
+          ),
+        ),
+      ),
+    );
+  }
+}
