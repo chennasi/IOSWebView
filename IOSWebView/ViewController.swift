81e7f5ca0078f9ed882f43b49211a223892f2fde
Chenna Subramani <chenna@chenna.co.in>
2026-02-10 06:38:23 +0530

    Update ViewController.swift

--- IOSWebView/ViewController.swift
+++ IOSWebView/ViewController.swift
@@ -1,1 +1,18 @@
+import UIKit
+import WebKit
 
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        view = webView
+        
+        let myURL = URL(string: "https://sb.shiprist.com") // Replace with your URL
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
